f7278975841ae6dcbfa987417d1e05a57747bde2
PJ1016 <41801479+PJ1016@users.noreply.github.com>
2024-05-16 10:33:17 +0530

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,27 @@ import Home from "./containers/Home";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const queryClient = new QueryClient();
+
+const isProduction = process.env.NODE_ENV === "production";
+const basename = isProduction ? "/Resume" : "/";
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/highestRuns",
-      element: <HighestRuns />,
-    },
-    {
-      path: "/player/:playerId",
-      element: <PlayerDetails />,
-    },
-  ]);
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/highestRuns",
+        element: <HighestRuns />,
+      },
+      {
+        path: "/player/:playerId",
+        element: <PlayerDetails />,
+      },
+    ],
+    { basename }
+  );
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router}></RouterProvider>
